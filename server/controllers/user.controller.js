13e70446ab52acf56606848fb125798519b2780b
John Alex Ladra <johnalexladra@gmail.com>
2023-09-09 02:25:50 +0800

    chore: add user controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,122 @@
+const { sequelize, Sequelize } = require("../models");
+const User = require("../models/user.model")(sequelize, Sequelize);
+const Op = Sequelize.Op;
+
+// get all users
+exports.getUsers = (req, res) => {
+    const firstName = req.query.firstName;
+    var condition = firstName ? { firstName: { [Op.like]: `%${firstName}%` } } : null;
+
+    User.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving users."
+            });
+        });
+};
+
+// get user using id
+exports.getUser = (req, res) => {
+    const id = req.params.id;
+
+    User.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find user with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving user with id=" + id
+            });
+        });
+};
+
+// create user
+exports.createUser = (req, res) => {
+    // Validate request
+    if (!req.body.firstName) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
+
+    // Create a User
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        // active: req.body.active ? req.body.active : false
+    };
+
+    // Save User in the database
+    User.create(user)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the user."
+            });
+        });
+}
+
+// delete user
+exports.deleteUser = (req, res) => {
+    const id = req.params.id;
+
+    User.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "User was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete user with id=${id}. Maybe user was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete user with id=" + id
+            });
+        });
+
+};
+
+// update user
+exports.updateUser = (req, res) => {
+    const id = req.params.id;
+
+    User.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "User was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update user with id=${id}. Maybe user was not found or request is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating user with id=" + id
+            });
+        });
+};

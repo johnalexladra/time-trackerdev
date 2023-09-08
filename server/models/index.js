daad0010fb49b45a1083c944bdbe9fc36c42f6d4
John Alex Ladra <johnalexladra@gmail.com>
2023-09-09 01:48:56 +0800

    chore: add model conn

---
+++ server/models/index.js
@@ -0,0 +1,25 @@
+const dbConfig = require("../config.js");
+
+// uses Sequelize a modern TypeScript and Node.js ORM
+const Sequelize = require("sequelize");
+
+// create an instances 
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// export
+module.exports = db;
